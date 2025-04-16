8d198404823f04817a817502fa8f7fa7e778ba24
soppinator <sophkas7674@gmail.com>
2025-04-16 19:41:19 -0400

    Add files via upload

---
+++ 1020-pg-64-lists-and-numbers/main.py
@@ -0,0 +1,23 @@
+num = ['zero', 'one', 'two', 'three', 'four']
+let = ['a', 'b', 'c', 'd', 'e']
+
+
+firstThree = "the first three items in the list are "
+mid = "three items from the middle of the list are "
+lastThree = "the last three items in the list are "
+letMsg = "the first five letters of the alphabet are "
+numMsg = "the first five numbers in the number system are " 
+
+
+print(letMsg)
+for letter in let:
+  print(letter)
+
+print(numMsg)
+for number in num:
+  print(number)
+
+
+
+
+
